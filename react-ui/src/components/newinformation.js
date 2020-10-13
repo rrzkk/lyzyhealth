1525f8061e04fe70083e69a161263ea8f1d742ca
ren <rrzkk@live.com>
2020-10-13 16:08:12 +1100

    information content

--- react-ui/src/components/newinformation.js
+++ react-ui/src/components/newinformation.js
@@ -10,28 +10,73 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import { Card } from 'reactstrap';
 import * as Scroll from 'react-scroll';
 import infoJson from './infoJson.json';
+import infoContent from './infoContentJson.json';
 
 
 
-const infocards = infoJson.map(el => {
+const infoCards = infoJson.map((el,index) => {
+   
     return (
         <div className="col-6 col-md-4">
+            <h>{index}</h><br/>
             <h>{el.figure}</h>
             <p>{el.text}</p>
         </div>
     );
-})
+});
 
+function symtoms(symptoms){
+    return symptoms.map(el=>{
+        return(
+        <div>
+            {el.text}    
+        </div>
+        );
+    });
+}
+function solution(solutions){
+    return solutions.map(el=>{
+        return(
+            <div>
+                <b>
+                    {el.title}
+                </b>
+                <p>
+                    {el.content}
+                </p>
+            </div>
+        );
+    });
+}
 
+const infoContentCards=infoContent.map(el=>{
+    return(
+        <div className="row">
+            <div className="col-12">
+                <b>{el.title}</b>
+                <p>{el.text}</p>
+            </div>
+            <div className="col-12 col-md-3">
+                <b>Symptoms</b>
+                {symtoms(el.symptoms)}
+            </div>
+            <div className="col-12 col-md-9">
+                <b>Prevention</b>
+                {solution(el.prevent)}
+            </div>
+        </div>
+    );
+});
 
 
 function NewInfo() {
-    console.log(infoJson);
+ 
     return (
-        <div className="container">
+        <div className="container infobackground">
             <div className="row">
-                {infocards}
+                {infoCards}
             </div>
+            {infoContentCards}
         </div>
     );
 }
