743ffe30541f3527c68b09a1355b57970e56112d
ren <rrzkk@live.com>
2020-10-22 18:17:00 +1100

    position fix

--- react-ui/src/components/challenge.js
+++ react-ui/src/components/challenge.js
@@ -160,7 +160,7 @@ function ChallengeCom(props) {
     const animatePos = [{ x: 0, y: -height / 2.8, scale: 1 }, { x: width / 6, y: -height / 1.8, scale: 1.2 }, { x: width / 2.6, y: -height / 3, scale: 1.3 }
         , { x: width * 2 / 3, y: -height / 1.2, scale: 1.4 }]
 
-    const animate = animatePos[count];
+    const animate = animatePos[props.userchallenge.userchallenge.length%3];
     function cycle() {
         dispatchC('MOVE');
     }
